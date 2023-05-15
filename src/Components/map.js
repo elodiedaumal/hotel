4ba411edd9887c3bacd7d1685631468b1e5cfedf
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-05-15 16:49:46 +0200

    delete old map component

--- src/Components/map.js
+++ src/Components/map.js
@@ -14,25 +14,8 @@ const myIcon = new L.Icon({
 });
 
 const Map = () => {
-  // const { isLoaded } = useLoadScript({
-  //   googleMapsApiKey: `${process.env.REACT_APP_GOOGLE_API_KEY}`,
-  // });
-
-  // const center = useMemo(() => ({ lat: 48.2261384, lng: -4.5042244 }), []);
   return (
     <div className='max-w-5xl mx-auto'>
-      {/* {!isLoaded ? (
-        <h1>Loading...</h1>
-      ) : (
-        <GoogleMap
-          mapContainerClassName='map-container'
-          center={center}
-          zoom={14}
-        >
-          <Marker position={center} />
-        </GoogleMap>
-      )} */}
-
       <MapContainer
         center={[48.2261384, -4.5042244]}
         zoom={13}
@@ -49,29 +32,5 @@ const Map = () => {
     </div>
   );
 };
-
-{
-  /* // <div className='max-w-5xl mx-auto'>
-  //   <p>why is my map not here</p>
-  //   <MapContainer
-  // {/* //     center={[48.2261384, -4.5042244]}
-  // //     zoom={13}
-  // //     scrollWheelZoom={true}
-  // //   >
-  // //     <TileLayer */
-}
-{
-  /* //       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  //       url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-  //     />
-  //     <Marker icon={myIcon} position={[48.2261384, -4.5042244]}>
-  //       <Popup>You are here!</Popup>
-  //     </Marker>
-  //   </MapContainer> */
-}
-{
-  /* //   <p>heloo</p>
-  // </div>; */
-}
 
 export default Map;
